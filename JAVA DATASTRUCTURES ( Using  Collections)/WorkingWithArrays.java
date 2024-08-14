8f49d7f47bee1c4cfdf272c1ba99a8f226c8986c
g_sakthivel <142934445+G-Sakthivel@users.noreply.github.com>
2024-08-14 20:38:22 +0530

    Update and rename Main.java to WorkingWithArrays.java

--- JAVA DATASTRUCTURES ( Using  Collections)/WorkingWithArrays.java
+++ JAVA DATASTRUCTURES ( Using  Collections)/WorkingWithArrays.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Main {
+public class WorkingWithArrays {
     public static void main(String [] args) {
 //Array initialization
         String[] colors = new String[5];
